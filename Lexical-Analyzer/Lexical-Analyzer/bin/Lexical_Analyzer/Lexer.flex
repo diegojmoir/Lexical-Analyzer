0a22434455ebbf3134a4ca251f386b711390025a
Diego Moir <djperezm@correo.url.edu.gt>
2018-10-11 11:05:32 -0600

    Moar errors fixed :J

---
+++ Lexical-Analyzer/Lexical-Analyzer/bin/Lexical_Analyzer/Lexer.flex
@@ -0,0 +1,54 @@
+package Lexical_Analyzer;
+import static Lexical_Analyzer.Token.*;
+
+%%
+
+%class Lexer
+%type Token
+%line
+%column
+
+%{
+    public String token; 
+    public int lineNumber; 
+    public int colNumber; 
+%}
+
+/*Patterns*/
+keyword = (void|int|double|bool|string|class|interface|null|this|extends|implements|for|while|if|else|return|break|New|NewArray)
+decimal = ([0-9]+)
+identifier = ([a-zA-Z])([a-zA-Z_decimal]*)//{0,30}
+singleLineComment = \/\/.+
+multiLineComment = "/*" ~"*/"
+bool = true|false
+hexadecimal = 0[xX][decimala-fA-F]+
+double = [0-9]+\.[0-9]*([eE][+-]?[0-9]+)?
+string = \".*?\"
+punctuationSymbols = (;|,|\.)
+error = (#|\´|\'|\_|\:|\$|\¿|\"|&|\||\~|\^|\`)
+twoCharOperators = (<=|>=|==|\!=|&&|\|\||\{\}|\[\]|\(\))
+oneCharOperators = (\+|\-|\*|\/|%|<|>|=|\!|\[|\]|\(|\)|\{|\})
+whiteSpace = \s
+%%
+
+/*Constants*/
+{bool} {token = yytext(); lineNumber = yyline; colNumber = yycolumn; return BOOL;}
+{double} {token = yytext(); lineNumber = yyline; colNumber = yycolumn; return DOUBLE;} 
+{string} {token = yytext(); lineNumber = yyline; colNumber = yycolumn; return STRING;}
+{decimal}|{hexadecimal} {token = yytext(); lineNumber = yyline; colNumber = yycolumn; return INTEGER;}
+
+{keyword} {token = yytext(); lineNumber = yyline;  colNumber = yycolumn; return KEYWORD;}
+{identifier} {token = yytext(); lineNumber = yyline; colNumber = yycolumn; return IDENTIFIER;}
+{singleLineComment}|{multiLineComment} {token = yytext(); lineNumber = yyline; colNumber = yycolumn; return COMMENT;}
+
+/*Punctuation Symbols*/
+{punctuationSymbols} {token = yytext(); lineNumber = yyline; colNumber = yycolumn; return PUNCTUATIONSYMBOL;}
+
+/*Validations*/
+{whiteSpace} {}
+{error} {token = yytext(); lineNumber = yyline; colNumber = yycolumn; return ERROR;}
+
+/*Operators and Characters*/
+{twoCharOperators} {token = yytext(); lineNumber = yyline; colNumber = yycolumn; return TWOCHAR;}
+{oneCharOperators} {token = yytext(); lineNumber = yyline; colNumber = yycolumn; return ONECHAR;}
+
---
+++ Lexical-Analyzer/Lexical-Analyzer/src/Lexical_Analyzer/Lexical_Analyzer.java
@@ -0,0 +1,82 @@
+package Lexical_Analyzer;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Lexical_Analyzer {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException, Exception {
+		// TODO Auto-generated method stub
+		 String path = new File(".").getAbsolutePath();
+		 String cupPath = path.substring(0, path.length()-1)  + "\\Lexical_Analyzer" + "\\parser.cup";
+	     path = path.substring(0, path.length()-1)  + "/Lexical_Analyzer" + "/Lexer.flex";
+ 
+
+	     String minicPath = "";
+	     boolean flag = true;
+	     Scanner sc = new Scanner(System.in); 
+	     while(flag) {
+		     System.out.println("Insert the path of the file..");
+		     minicPath = sc.nextLine();    
+		     
+	    	 if(new File(minicPath).exists()) {
+			     GenerateCup(cupPath);
+			     GenerateJflex(path);
+	    		 CheckLex(new File(minicPath));
+	    		 flag = false; 
+	    	 }
+	     }
+	    
+	}
+	
+	
+	public static String getFormat(String text, int line, int column, String token) {
+		String result = text + "      Line:" + (line+1) + "      Columns:" + (column+1) + "-" + (column+text.length()) +  "      is: " + token;
+        return result;
+	}
+
+	public static void GenerateJflex(String path) {
+		File file = new File(path);
+		jflex.Main.generate(file);
+	}
+	
+	public static void GenerateCup(String path) {
+
+		String pathCup = path.substring(0, path.length() - 11);
+		String[] commands = {/* "-expect", "32" ,*/"-destdir", pathCup ,"-parser", "parser", path};
+		try {
+			java_cup.Main.main(commands);
+		}catch(Exception ex) {
+			
+		}
+	}
+
+	public static void CheckLex(File file) throws FileNotFoundException, IOException, Exception{
+        String path = file.getPath();
+        boolean flag = true; 
+        Reader reader = new BufferedReader(new FileReader(path));
+        Lexer lex = new Lexer(reader);
+        parser p = new parser(lex);
+        
+        //p.debug_parse();
+        p.parse();
+        if((p.numErr + lex.numLErr) == 0) {
+        	System.out.println("Archivo sintacticamente correcto :J");
+        }
+        
+    }
+    
+     
+    
+
+}
